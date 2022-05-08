5f0946905b6be9ec02f8258d700600e01a00bc43
narendra175 <105099837+narendra175@users.noreply.github.com>
2022-05-08 14:34:34 +0530

    Create pylint.py

---
+++ pylint.py
@@ -0,0 +1,23 @@
+import string
+
+SHIFT = 3
+CHOICE = input("would you like to encode or decode?")
+WORD = input("Please enter text")
+LETTERS = string.ascii_letters + string.punctuation + string.digits
+ENCODED = ''
+if CHOICE == "ENCODED":
+    for LETTER in WORD:
+        if LETTER == ' ':
+            ENCODED = ENCODED + ' '
+        else:
+            x = LETTERS.index(LETTER) + SHIFT
+            ENCODED = ENCODED + LETTERS[x]
+if CHOICE == "decode":
+    for LETTER in WORD:
+        if LETTER == ' ':
+            ENCODED = ENCODED + ' '
+        else:
+            x = LETTERS.index(LETTER) - SHIFT
+            ENCODED = ENCODED + LETTERS[x]
+
+print(ENCODED)
